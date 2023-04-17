b328c1e9390b06fc83502ecfacbd9f6121d23fae
hirakero <k16.h.white@gmail.com>
2023-04-17 01:34:42 +0000

    improve: boundary.users の sqlを文字列からhoneysqlに変更

--- src/clj/isomorphic_clojure_webapp/boundary/users.clj
+++ src/clj/isomorphic_clojure_webapp/boundary/users.clj
@@ -1,7 +1,9 @@
 (ns isomorphic-clojure-webapp.boundary.users 
   (:require [duct.database.sql]
             [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as rs])
+            [next.jdbc.result-set :as rs]
+            [honey.sql :as sql]
+            [honey.sql.helpers :as hh])
   (:import [java.sql SQLException]))
 
 (defprotocol Users
@@ -16,25 +18,40 @@
 (extend-protocol Users
   duct.database.sql.Boundary
   (get-user-by-id [db id] 
-    (let [ds (-> db :spec :datasource)
-          result (jdbc/execute! ds ["SELECT * FROM users WHERE id = ?" id] execute-opts)]
+    (let [ds (-> db :spec :datasource) 
+          result (jdbc/execute! ds
+                                (sql/format (-> (hh/select :*)
+                                                (hh/from :users)
+                                                (hh/where := :id id)))
+                                execute-opts)]
       result))
+  
   (create-user [db values] 
     (try
       (let [ds (-> db :spec :datasource)
-            result (jdbc/execute! ds ["INSERT INTO users (name) VALUES (?)" (:name values)] execute-opts)]
+            result (jdbc/execute! ds
+                                  (sql/format (-> (hh/insert-into :users [:name])
+                                                  (hh/values [[(:name values)]])))
+                                  execute-opts)]
         result)
       (catch SQLException e         
           ;TODO log
-          ;TODO ex-info  
-        ))) 
+          ;TODO ex-info 
+        )))
   
   (update-user [db id values]
     (let [ds (-> db :spec :datasource)
-          result (jdbc/execute! ds ["UPDATE users SET name = ? WHERE id = ?" (:name values)  id] execute-opts)]
+          result (jdbc/execute! ds 
+                                (sql/format (-> (hh/update :users)
+                                                (hh/set {:name (:name values)})
+                                                (hh/where [:= :id id])))
+                                execute-opts)]
       result))
+  
   (delete-user [db id] 
-    (let [ds (-> db :spec :datasource)
-          result (jdbc/execute! ds ["DELETE FROM users WHERE id = ?" id] execute-opts)]
-      result      )             
-               ))
+               (let [ds (-> db :spec :datasource)
+                     result (jdbc/execute! ds
+                                           (sql/format (-> (hh/delete-from :users)
+                                                           (hh/where [:= :id id])))
+                                           execute-opts)]
+                 result)))
