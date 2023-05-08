2bab2e36f0e767ce836cb1c270031dfbd169eb02
hirakero <k16.h.white@gmail.com>
2023-05-08 08:31:59 +0000

    feat: handler/products を追加

--- src/clj/isomorphic_clojure_webapp/handler/products.clj
+++ src/clj/isomorphic_clojure_webapp/handler/products.clj
@@ -1,13 +1,48 @@
 (ns isomorphic-clojure-webapp.handler.products
-  (:require [integrant.core :as ig]))
+  (:require [integrant.core :as ig]
+            [isomorphic-clojure-webapp.boundary.products :as products]))
 
 (defmethod ig/init-key ::all [_ {:keys [db]}]
-  (fn [req]))
+  (fn [req]
+    (let [result (products/get-products db {})]
+      (if (empty? result)
+        {:status 404
+         :body {:products []}}
+        {:status 200
+         :body {:products result}}))))
+
 (defmethod ig/init-key ::fetch [_ {:keys [db]}]
-  (fn [req]))
+  (fn [{:keys [path-params]}]
+    (let [result (products/get-product-by db :id (:product-id path-params))]
+      (if (empty? result)
+        {:status 404
+         :body {:product nil}}
+        {:status 200
+         :body {:product result}}))))
+
 (defmethod ig/init-key ::create [_ {:keys [db]}]
-  (fn [req]))
+  (fn [{:keys [body-params]}]
+    (let [result (products/create-product db body-params)]
+      (if (empty? result)
+        {:status 400
+         :body {:product nil}}
+        {:status 201
+         :body {:product result}}))))
+
 (defmethod ig/init-key ::update [_ {:keys [db]}]
-  (fn [req]))
+  (fn [{:keys [path-params body-params]}]
+    (let [result (products/update-product db (:product-id path-params) body-params)]
+      (if (empty? result)
+        {:status 404
+         :body {:product nil}}
+        {:status 200
+         :body {:product result}}))))
+
 (defmethod ig/init-key ::delete [_ {:keys [db]}]
-  (fn [req]))
+  (fn [{:keys [path-params]}]
+    (let [result (products/delete-product db (:product-id path-params))]
+      (if (empty? result)
+        {:status 404
+         :body {}}
+        {:status 204
+         :body {}}))))
---
+++ test/clj/isomorphic_clojure_webapp/handler/products_test.clj
@@ -0,0 +1,74 @@
+(ns isomorphic-clojure-webapp.handler.products-test
+  (:require [clojure.test :refer [deftest testing is are use-fixtures]]
+            [isomorphic-clojure-webapp.test-helper :as helper]
+            [integrant.core :as ig]
+            [integrant.repl.state :refer [system config]]
+            [next.jdbc :as jdbc]
+            [isomorphic-clojure-webapp.handler.products :as sut]))
+
+(comment
+  (let [boundary (:duct.database.sql/hikaricp system)
+        handler (ig/init-key :isomorphic-clojure-webapp.handler.products/create boundary)]
+    (handler {:request-method :post :body {:name "abc"}})))
+
+(use-fixtures :each
+  (fn [f]
+    (let [boundary (:duct.database.sql/hikaricp system)
+          ds (-> boundary :spec :datasource)]
+      (jdbc/execute! ds ["delete from products"])
+      #_(jdbc/execute! ds ["insert into products (name, description) 
+                          values ('hammer',''),
+                                 ('bean flip',''),
+                                 ('scout','')"]))
+
+    (f)))
+
+(deftest handler-products-test
+  (testing "get /products データがない場合は空のベクタを返す"
+    (let [{:keys [status body] :as all} (helper/http-get "/products")]
+      (is (=  404 status))
+      (is (=  [] (:products body)))))
+  (let [{:keys [status body]} (helper/http-post "/products"
+                                                {:name "Hammer XT"
+                                                 :description "for hammer grip"})
+        product (:product body)
+        id (:id product)]
+    (testing "post /products 登録した内容を返す"
+      (is (= status 201))
+      (is (= "Hammer XT" (:name product)))
+      (is (= "for hammer grip" (:description product))))
+
+    (testing "get /products/:product-id "
+      (testing "取得した内容を返す"
+        (let [{:keys [status body] :as all} (helper/http-get (str "/products/" id))]
+          (is (= 200 status))
+          (is (= "Hammer XT" (-> body :product :name)))))
+
+      (testing "対象データが無ければ not found"
+        (let [{:keys [status body] :as all} (helper/http-get (str "/products/00000000-0000-0000-0000-000000000000"))]
+          (is (= 404 status)))))
+
+    (testing "put /products/:product-id"
+      (testing "更新した内容を返す"
+        (let [{:keys [status body]} (helper/http-put (str "/products/" id) {:name "Hammer LT"})]
+          (is (= 200 status))
+          (is (= "Hammer LT" (-> body :product :name)))))
+      (testing "対象データが無ければ not found"
+        (let [{:keys [status body]} (helper/http-put "/products/00000000-0000-0000-0000-000000000000" {:name "Hammer LT"})]
+          (is (= 404 status)))))
+
+    (testing "get /products データの配列を返す"
+      (helper/http-post "/products"
+                        {:name "axiom ocuralis"
+                         :description "ocuralis plug"})
+      (let [{:keys [status body]} (helper/http-get "/products")]
+        (is (= 200 status))
+        (is (= 2 (-> body :products count)))))
+
+    (testing "delete"
+      (testing "削除に成功したらno content"
+        (let [{:keys [status body]} (helper/http-delete (str "/products/" id))]
+          (is (= 204 status))))
+      (testing "対象データが無ければ not found"
+        (let [{:keys [status body]} (helper/http-delete "/products/00000000-0000-0000-0000-000000000000")]
+          (is (= 404 status)))))))
