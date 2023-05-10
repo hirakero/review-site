41b644f13aa661ff5ce4e07faaa4226e5a578133
hirakero <k16.h.white@gmail.com>
2023-05-10 12:50:13 +0000

    feat: productのUI作成途中

--- src/clj/isomorphic_clojure_webapp/ui/handler/products.clj
+++ src/clj/isomorphic_clojure_webapp/ui/handler/products.clj
@@ -1,6 +1,7 @@
 (ns isomorphic-clojure-webapp.ui.handler.products
   (:require [integrant.core :as ig]
-            [rum.core :as rum]))
+            [rum.core :as rum]
+            [clj-http.client :as client]))
 
 (defn common [& body]
   {:status 200
@@ -8,25 +9,73 @@
    :body (rum/render-html [:html
                            [:body body]])})
 
+(def ^:private base-url "http://localhost:3000")
+
+(defn http-get [path]
+  (client/get (str base-url path)
+              {:accept :json
+               :as :json
+               :coerce :always
+               :throw-exceptions? false}))
+
+(defn http-post [path body]
+  (client/post (str base-url path)
+               {:form-params body
+                :content-type :json
+                :accept :json
+                :as :json
+                :coerce :always
+                :throw-exceptions? false}))
+
+(comment
+  (-> (http-get "/api/products")
+      :body
+      :products))
 
 (defmethod ig/init-key ::list [_ _]
   (fn [req]
-    (common
-     [:h2 "product list"]
-     [:ul (for [i (range 5)]
-            [:li i])])))
+    (let [products (-> (http-get "/api/products")
+                       :body
+                       :products)]
+      (common [:h2 "product list"]
+              [:ul (for [{:keys [name id]} products]
+                     [:li [:a {:href (str "/ui/products/detail/" id)} name]])]))))
 
 (defmethod ig/init-key ::detail [_ _]
-  (fn [req]
-    (common [:h2 "product fetch"])))
+  (fn [{:keys [path-params]}]
+    (let [{:keys [name description]} (-> (http-get (str "/api/products/" (:product-id path-params)))
+                                         :body
+                                         :product)]
+      (common [:h2 "product detail"]
+              [:p "name: " name]
+              [:p "description: " description]
+              [:p [:a {:href "/ui/products"} "product list"]]))))
 
 (defmethod ig/init-key ::create [_ _]
   (fn [req]
-    (common [:h2 "product create"])))
+    (common [:h2 "product create"]
+            [:form {:method :post :action ""}
+             [:div
+              [:label {:for "name"} "name"]
+              [:input {:type :text :name "name" :id "name"}]]
+             [:div
+              [:label {:for "description"} "description"]
+              [:input {:type :text :name "description" :id "description"}]]
+             [:div
+              [:button {:type :submit} "send"]]])))
 
 (defmethod ig/init-key ::create-post [_ _]
-  (fn [req]
-    (common [:h2 "product create post"])))
+  (fn [{:keys [form-params] :as all}]
+    (let [{:strs [name description]} form-params]
+      (if-let [result (-> (http-post "/api/products" {:name name
+                                                      :description description})
+                          :body
+                          :product)]
+        (common [:h2  "product created"]
+                [:div (str "name: " name)]
+                [:div (str "description: " description)]) ;redirect
+        (common [:h2  "product create"]
+                [:div "error"])))))
 
 (defmethod ig/init-key ::edit [_ _]
   (fn [req]
@@ -43,3 +92,5 @@
 (defmethod ig/init-key ::delete-post [_ _]
   (fn [req]
     (common [:h2 "product delete post"])))
+
+
