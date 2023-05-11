e26c19956ecdc3cccdb2add226aa0dab7a37df5d
hirakero <k16.h.white@gmail.com>
2023-05-11 02:00:13 +0000

    feat: productのui作成途中

--- src/clj/isomorphic_clojure_webapp/ui/handler/products.clj
+++ src/clj/isomorphic_clojure_webapp/ui/handler/products.clj
@@ -1,7 +1,7 @@
 (ns isomorphic-clojure-webapp.ui.handler.products
   (:require [integrant.core :as ig]
             [rum.core :as rum]
-            [clj-http.client :as client]))
+            [isomorphic-clojure-webapp.ui.boundary.http-helper :as helper]))
 
 (defn common [& body]
   {:status 200
@@ -9,47 +9,27 @@
    :body (rum/render-html [:html
                            [:body body]])})
 
-(def ^:private base-url "http://localhost:3000")
-
-(defn http-get [path]
-  (client/get (str base-url path)
-              {:accept :json
-               :as :json
-               :coerce :always
-               :throw-exceptions? false}))
-
-(defn http-post [path body]
-  (client/post (str base-url path)
-               {:form-params body
-                :content-type :json
-                :accept :json
-                :as :json
-                :coerce :always
-                :throw-exceptions? false}))
-
-(comment
-  (-> (http-get "/api/products")
-      :body
-      :products))
-
 (defmethod ig/init-key ::list [_ _]
   (fn [req]
-    (let [products (-> (http-get "/api/products")
+    (let [products (-> (helper/http-get "/api/products")
                        :body
                        :products)]
       (common [:h2 "product list"]
               [:ul (for [{:keys [name id]} products]
-                     [:li [:a {:href (str "/ui/products/detail/" id)} name]])]))))
+                     [:li [:a {:href (str "/ui/products/detail/" id)} name]])]
+              [:a {:href "/ui/products/create"} "create"]))))
 
 (defmethod ig/init-key ::detail [_ _]
-  (fn [{:keys [path-params]}]
-    (let [{:keys [name description]} (-> (http-get (str "/api/products/" (:product-id path-params)))
+  (fn [{{:keys [:product-id]} :path-params}]
+    (let [{:keys [name description]} (-> (helper/http-get (str "/api/products/" product-id))
                                          :body
                                          :product)]
       (common [:h2 "product detail"]
-              [:p "name: " name]
-              [:p "description: " description]
-              [:p [:a {:href "/ui/products"} "product list"]]))))
+              [:div (str "name: " name)]
+              [:div (str "description: " description)]
+
+              [:div [:a {:href (str "/ui/products/edit/" product-id)} "edit"] " / " [:a {:href (str "/ui/products/delete/" product-id)} "delete"]]
+              [:div [:a {:href "/ui/products"} "product list"]]))))
 
 (defmethod ig/init-key ::create [_ _]
   (fn [req]
@@ -62,35 +42,75 @@
               [:label {:for "description"} "description"]
               [:input {:type :text :name "description" :id "description"}]]
              [:div
-              [:button {:type :submit} "send"]]])))
+              [:button {:type :submit} "create"]]])))
 
 (defmethod ig/init-key ::create-post [_ _]
-  (fn [{:keys [form-params] :as all}]
+  (fn [{:keys [form-params]}]
     (let [{:strs [name description]} form-params]
-      (if-let [result (-> (http-post "/api/products" {:name name
-                                                      :description description})
+      (if-let [result (-> (helper/http-post "/api/products" {:name name
+                                                             :description description})
                           :body
                           :product)]
         (common [:h2  "product created"]
                 [:div (str "name: " name)]
-                [:div (str "description: " description)]) ;redirect
+                [:div (str "description: " description)]) ;redirect 303 (See Other)
         (common [:h2  "product create"]
                 [:div "error"])))))
 
 (defmethod ig/init-key ::edit [_ _]
-  (fn [req]
-    (common [:h2 "product edit"])))
+  (fn [{:keys [path-params]}]
+    (println "pp " path-params)
+    (if-let [result (-> (helper/http-get (str "/api/products/" (:product-id path-params)))
+                        :body
+                        :product)]
+      (common [:h2 "product edit"]
+              [:form {:method :post :action ""}
+               [:div
+                [:label {:for "name"} "name"]
+                [:input {:type :text :name "name" :id "name" :value (:name result)}]]
+               [:div
+                [:label {:for "description"} "description"]
+                [:input {:type :text :name "description" :id "description" :value (:description result)}]]
+               [:div
+                [:button {:type :submit} "update"]]])
+      (common [:h2 "product edit"]
+              [:div "not found"]))))
 
 (defmethod ig/init-key ::edit-post [_ _]
-  (fn [req]
-    (common [:h2 "product edit post"])))
+  (fn [{:keys [form-params path-params]}]
+    (let [{:strs [name description]} form-params]
+      (if-let [result (-> (helper/http-put (str "/api/products/" (:product-id path-params)) {:name name
+                                                                                             :description description})
+                          :body
+                          :product)]
+        (common [:h2  "product edit"]
+                [:div (str "name: " name)]
+                [:div (str "description: " description)]) ;redirect 303 (See Other)
+        (common [:h2  "product edit"]
+                [:div "error"])))))
 
 (defmethod ig/init-key ::delete [_ _]
-  (fn [req]
-    (common [:h2 "product delete"])))
+  (fn [{:keys [path-params]}]
+    (if-let [result (-> (helper/http-get (str "/api/products/" (:product-id path-params)))
+                        :body
+                        :product)]
+      (common [:h2 "product delete"]
+              [:form {:method :post :action ""}
+               [:div
+                [:p "name: " (:name result)]]
+               [:div
+                [:p "description: " (:description result)]]
+               [:div
+                [:button {:type :submit} "delete"]]])
+      (common [:h2  "product delete"]
+              [:div "not found"]))))
 
 (defmethod ig/init-key ::delete-post [_ _]
-  (fn [req]
-    (common [:h2 "product delete post"])))
-
+  (fn [{:keys [path-params]}]
+    (let [{:keys [status body]} (-> (helper/http-delete (str "/api/products/" (:product-id path-params))))]
+      (if (= 204 status)
+        (common [:h2 "product delete"]
+                [:div "success"])
+        (common [:h2 "product delete post"]
+                [:div "error"])))))
 
