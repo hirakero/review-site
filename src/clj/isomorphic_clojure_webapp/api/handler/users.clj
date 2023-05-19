ae6ff2d2b19c2c98166e0df05964e2ad5f1bff60
hirakero <k16.h.white@gmail.com>
2023-05-19 08:23:21 +0000

    :update handler.usersのレスポンスを変更

--- src/clj/isomorphic_clojure_webapp/api/handler/users.clj
+++ src/clj/isomorphic_clojure_webapp/api/handler/users.clj
@@ -7,7 +7,7 @@
   (fn [req]
     (let [result (users/get-users db)]
       (if (empty? result)
-        (rres/not-found {:users []})
+        (rres/not-found nil)
         (rres/response {:users result})))))
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
@@ -15,29 +15,28 @@
     (let [result (users/create-user db body-params)]
       (if (empty? result)
         (rres/bad-request {:user nil})
-        (rres/created (str "/api/users/" (:id result)) {:user result})))))
+        (rres/created (str "/api/users/" (:id result)) result)))))
 
 (defmethod ig/init-key ::fetch [_ {:keys [db]}]
   (fn [{:keys [path-params]}]
     (let [result (users/get-user-by db :id (:user-id path-params))]
       (if (empty? result)
-        (rres/not-found {:user nil})
+        (rres/not-found nil)
         (rres/response {:user result})))))
 
 (defmethod ig/init-key ::update [_ {:keys [db]}]
   (fn [{:keys [path-params body-params]}]
     (let [result (users/update-user db (:user-id path-params) body-params)]
       (if (empty? result)
-        (rres/not-found {:user nil})
-        (rres/response {:user result})))))
+        (rres/not-found nil)
+        (rres/response  result)))))
 
 (defmethod ig/init-key ::delete [_ {:keys [db]}]
   (fn [{:keys [path-params]}]
     (let [result (users/delete-user db (:user-id path-params))]
       (if (empty? result)
-        (rres/not-found {})
-        {:status 204
-         :body {}}))))
+        (rres/not-found nil)
+        {:status 204}))))
 
 (defmethod ig/init-key ::signin [_ {:keys [db]}]
   (fn [{:keys [body-params]}]
