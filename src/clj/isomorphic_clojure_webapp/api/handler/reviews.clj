a06acd6ee554aedad2a9a1c393c7e885dd6e76e0
hirakero <k16.h.white@gmail.com>
2023-07-15 05:01:53 +0000

    feat: handler.reivews 作成途中

--- src/clj/isomorphic_clojure_webapp/api/handler/reviews.clj
+++ src/clj/isomorphic_clojure_webapp/api/handler/reviews.clj
@@ -1,6 +1,8 @@
 (ns isomorphic-clojure-webapp.api.handler.reviews
   (:require [integrant.core :as ig]
             [isomorphic-clojure-webapp.api.boundary.reviews :as reviews]
+            [isomorphic-clojure-webapp.api.boundary.products :as products]
+            [isomorphic-clojure-webapp.api.boundary.users :as users]
             [ring.util.response :as rres]))
 
 (defmethod ig/init-key ::list [_ {:keys [db]}]
@@ -13,11 +15,17 @@
 (defmethod ig/init-key ::create [_ {:keys [db]}]
   (fn [{:keys [body-params path-params identity]}]
     (let [{:keys [product-id]} path-params
-          user-id (:sub identity)]
-      (let [params (assoc body-params :product-id product-id :user-id user-id)]
-        (if-let [result (reviews/create-review db params)]
-          (rres/created (str "/api/reviews/" (:id result)) result)
-          (rres/bad-request {:error ""}))))))
+          user-id (:sub identity)
+          product (products/get-product-by db :id product-id)
+          user (users/get-user-by db :id user-id)]
+      (if-not product
+        (rres/not-found {:error "product not found"})
+        (if-not user
+          (rres/not-found {:error "user not found"})
+          (let [params (assoc body-params :product-id product-id :user-id user-id)]
+            (if-let [result (reviews/create-review db params)]
+              (rres/created (str "/api/reviews/" (:id result)) result)
+              (rres/bad-request {:error ""}))))))))
 
 #_(defmethod ig/init-key ::update [_ {:keys [db]}]
     (fn [req]))
@@ -26,14 +34,13 @@
   (fn [{:keys [path-params identity]}]
     (let [review-id (:review-id path-params)
           target (reviews/get-review-by-id db review-id)
-          user-id (:sub identity)
-          _ (println "\ntarget " target)
-          _ (println "\npp " path-params)]
+          user-id (:sub identity)]
+      ;TODO 対象データがなくても成功にする？
       (if (nil? target)
-        (rres/not-found {:error "not-found"})
+        (rres/not-found {:error "review not found"})
         (if (= (-> target :user-id str) user-id)
           (if-let [result (reviews/delete-review db review-id)]
             {:status 204}
-            (rres/not-found {:error "not-found"}))
+            (rres/not-found {:error "review not found"}))
           {:status 403
-           :body {}})))))
+           :body {:error "permission error"}})))))
--- test/clj/isomorphic_clojure_webapp/api/handler/reviews_test.clj
+++ test/clj/isomorphic_clojure_webapp/api/handler/reviews_test.clj
@@ -15,114 +15,140 @@
     (f)))
 
 (deftest handler-reviews-test
+  (println "\nhandler reviews test")
   (let [;正規ユーザー追加
-        {{{user-id :id
-           user-name :name} :user} :body} (helper/http-post "/api/signup"
-                                                            {:name "chris"
-                                                             :email "chris@email.com"
-                                                             :password "password"})
+        _  (helper/http-post "/api/signup"
+                             {:name "Chris"
+                              :email "chris@email.com"
+                              :password "password"})
         ;サインイン、トークン保持
         {{:keys [token]} :body} (helper/http-post "/api/signin"
-                                                  {:name "chris"
+                                                  {:name "Chris"
                                                    :email "chris@email.com"
                                                    :password "password"})
+        token-header {"authorization" (str "Token " token)}
 
         ;他のユーザー追加
-        {{{other-user-id :id
-           other-user-name :name} :user} :body} (helper/http-post "/api/signup"
-                                                                  {:name "Dave"
-                                                                   :email "dave@email.com"
-                                                                   :password "password"})
+        _  (helper/http-post "/api/signup"
+                             {:name "Dave"
+                              :email "dave@email.com"
+                              :password "password"})
+
         {{other-token :token} :body} (helper/http-post "/api/signin"
                                                        {:name "Dave"
                                                         :email "dave@email.com"
                                                         :password "password"})
+
+        other-token-header {"authorization" (str "Token " other-token)}
+
         ;商品追加
         {{product-id :id} :body} (helper/http-post "/api/products"
                                                    {:name "sr400"
-                                                    :description "single 400cc"})]
+                                                    :description "single 400cc"})
+        result (helper/http-post (str "/api/products/" product-id "/reviews")
+                                 token-header
+                                 {:title "not bad"
+                                  :content "Today, I wanna show you ...."
+                                  :rate 4})
+        review-id (-> result :body :id)]
+    (testing "post /products/:product-id/reviews"
+      (testing "正常。201 とlocationと、登録した内容を直接返す"
+        (let [{:keys [status body headers]} result]
+          (is (= 201 status))
+          (is (match? {:title "not bad"
+                       :content "Today, I wanna show you ...."
+                       :rate 4}
+                      body))
+          (is (clojure.string/includes? (get headers "location") (:id body)))
+          (testing "path-paramsのproduct-idが登録されている"
+            (is (= product-id (:product-id body))))
 
-    (let [result (helper/http-post (str "/api/products/" product-id "/reviews")
-                                   {"authorization" (str "Token " token)}
-                                   {:title "not bad"
-                                    :content "Today, I wanna show you ...."
-                                    :rate 4})
-          review-id (-> result :body :id)]
-      (testing "post /products/:product-id/reviews"
+          (testing "tokenのuser-id(sub)が登録されている"
+            (is (= (-> token auth/parse-token :sub) (:user-id body))))))
 
-        (testing "正常。201 とロケーションと、登録した内容を直接返す"
-          (let [{:keys [status body headers]} result]
-            (is (= 201 status))
-            (is (match? {:title "not bad"
-                         :content "Today, I wanna show you ...."
-                         :rate 4}
-                        body))
-            (is (clojure.string/includes? (get headers "location") (:id body)))
-            (testing "path-paramsのproduct-idが登録されている"
-              (is (= product-id (:product-id body))))
-
-            (testing "tokenのuser-id(sub)が登録されている"
-              (is (= (-> token auth/parse-token :sub) (:user-id body))))))
-
-        (testing "空bodyは 400")
-        (testing "フィールドの型が合わない場合は 400")
-        (testing "フィールドが足りない場合は 400")
-        (testing "ユーザーが存在しない 404?")
-        (testing "product が存在しない 404?"))
-
-      (testing " get /products/:product-id/reviews")
-      (testing " get /users/:user-id/reviews")
-      (testing " get /reviews query")
-
-      (testing "get /reviews/:review-id"
-        (testing "取得した内容を返す :review {,,,}")
-        (testing "対象データが無ければ 404 で, :review nilを返す")
-        (testing ":id がUUIDでなければ 400 とメッセージ"))
-
-      (testing "get /reviews"
-        (testing "データの配列を返す")
-        (testing "クエリパラメータで絞り込み"
-          (testing "対象データが無ければ404で、:products nilを返す"))
-        (testing "クエリパラメータでページネーション"
-          (testing "offset,limit 数値以外は400"))
-        (testing "クエリパラメータでソート"
-          (testing "")
-          (testing "orderのasc,desc以外は400")))
-      (testing " update は無し"
-         ;405
+      (testing "tokenなしは401"
+        (let [{:keys [status body]} (helper/http-post (str "/api/products/" product-id "/reviews")
+                                                      {:title "awsome"
+                                                       :content "tons of pros ..."
+                                                       :rate 5})]
+          (is (= 401 status))))
+      (testing "フィールドが足りない場合は 400"
+        (let [{:keys [status body]} (helper/http-post (str "/api/products/" product-id "/reviews")
+                                                      token-header
+                                                      {:title "not bad"
+                                                       :rate 4})]
+          (is (= 400 status))))
+      (testing "フィールドの型が合わない場合は 400"
+        (let [{:keys [status body]} (helper/http-post (str "/api/products/" product-id "/reviews")
+                                                      token-header
+                                                      {:title "excellent"
+                                                       :content ""
+                                                       :rate "good!"})]
+          (is (= 400 status))))
+      (testing "ユーザーが存在しない 404?"
+          ;そもそもtokenが発行されないはず？
+          ;ログイン後にユーザー削除の可能性も？
         )
+      (testing "product が存在しない 404?"
+        (let [{:keys [status body]} (helper/http-post (str "/api/products/" "00000000-0000-0000-0000-000000000000" "/reviews")
+                                                      token-header
+                                                      {:title "awsome"
+                                                       :content "tons of pros ..."
+                                                       :rate 5})]
+          (is (= 404 status)))))
 
-      (testing "delete"
-        (let [token-header {"authorization" (str "Token " token)}]
-          (testing "認証なしは401"
-            (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" review-id))]
-              (is (= 401 status))))
+    (testing " get /products/:product-id/reviews")
+    (testing " get /users/:user-id/reviews")
+    (testing " get /reviews query")
 
-          (testing "本人以外は消せない 403"
-            (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" review-id)
-                                                            {"authorization" (str "Token " other-token)})]
-              (is (= 403 status))))
+    (testing "get /reviews/:review-id"
+      (testing "取得した内容を返す :review {,,,}")
+      (testing "対象データが無ければ 404 で, :review nilを返す")
+      (testing ":id がUUIDでなければ 400 とメッセージ"))
 
-          (helper/http-delete (str "/api/reviews/" "00000000-0000-0000-0000-000000000000")
-                              {"authorization" (str "Token " "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOm51bGwsIm5hbWUiOiJjaHJpcyIsImV4cCI6MTY4OTIzMjIzOH0.ke_IHSWje7T3HySP87PqZDDhHtXqI1KIhi1DUiuXVL8")})
+    (testing "get /reviews"
+      (testing "データの配列を返す")
+      (testing "クエリパラメータで絞り込み"
+        (testing "対象データが無ければ404で、:products nilを返す"))
+      (testing "クエリパラメータでページネーション"
+        (testing "offset,limit 数値以外は400"))
+      (testing "クエリパラメータでソート"
+        (testing "")
+        (testing "orderのasc,desc以外は400")))
+    (testing " update は無し"
+         ;405
+      )
 
-          (testing "対象データが無ければ 404"
-            (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" "00000000-0000-0000-0000-000000000000")
-                                                            token-header)]
-              (is (= 404 status))))
+    (testing "delete"
+      (testing "認証なしは401"
+        (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" review-id))]
+          (is (= 401 status))))
 
-          (testing ":review-id  無しは405"
-            (let [{:keys [status body]} (helper/http-delete "/api/reviews"
-                                                            token-header)]
-              (is (= 405 status))))
+      (testing "本人以外は消せない 403"
+        (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" review-id)
+                                                        other-token-header)]
+          (is (= 403 status))))
 
-          (testing ":review-idの型が違っていれば400"
-            (let [{:keys [status body]} (helper/http-get (str "/api/reviews/" "abc")
-                                                         token-header)]
-              (is (= 400 status))))
+      #_(helper/http-delete (str "/api/reviews/" "00000000-0000-0000-0000-000000000000")
+                            {"authorization" (str "Token " "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOm51bGwsIm5hbWUiOiJjaHJpcyIsImV4cCI6MTY4OTIzMjIzOH0.ke_IHSWje7T3HySP87PqZDDhHtXqI1KIhi1DUiuXVL8")})
 
-          (testing "削除に成功したらno contentで何も返さない"
-            (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" review-id)
-                                                            token-header)]
-              (is (= 204 status))
-              (is (nil? body)))))))))
+      (testing "対象データが無ければ 404"
+        (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" "00000000-0000-0000-0000-000000000000")
+                                                        token-header)]
+          (is (= 404 status))))
+
+      (testing ":review-id  無しは405"
+        (let [{:keys [status body]} (helper/http-delete "/api/reviews"
+                                                        token-header)]
+          (is (= 405 status))))
+
+      (testing ":review-idの型が違っていれば400"
+        (let [{:keys [status body]} (helper/http-get (str "/api/reviews/" "abc")
+                                                     token-header)]
+          (is (= 400 status))))
+
+      (testing "削除に成功したらno contentで何も返さない"
+        (let [{:keys [status body]} (helper/http-delete (str "/api/reviews/" review-id)
+                                                        token-header)]
+          (is (= 204 status))
+          (is (nil? body)))))))
