4a5d3e6b46227389b6c31c3f0b532589d0135a2e
hirakero <k16.h.white@gmail.com>
2023-08-04 05:57:17 +0000

    fix: see-other置き換え

--- src/clj/isomorphic_clojure_webapp/ui/handler/products.clj
+++ src/clj/isomorphic_clojure_webapp/ui/handler/products.clj
@@ -1,7 +1,8 @@
 (ns isomorphic-clojure-webapp.ui.handler.products
   (:require [integrant.core :as ig]
-            [isomorphic-clojure-webapp.ui.response :refer [ok see-other]]
-            [isomorphic-clojure-webapp.ui.boundary.products :as products]))
+            [isomorphic-clojure-webapp.ui.response :refer [ok]]
+            [isomorphic-clojure-webapp.ui.boundary.products :as products]
+            [ring.util.http-response :as res]))
 
 
 (defmethod ig/init-key ::list [_ {:keys [host]}]
@@ -63,7 +64,7 @@
     (let [{:strs [name description]} form-params]
       (let [{:keys [status]} (products/create-product host form-params)]
         (if (= status 201)
-          (see-other (str "/products"))
+          (res/see-other (str "/products"))
           (ok [:h2  "product create"]
               [:div "error"]))))))
 
@@ -86,7 +87,7 @@
   (fn [{:keys [form-params path-params]}]
     (let [{:keys [status body]}  (products/update-product host (:product-id path-params) form-params)]
       (if (= status 200)
-        (see-other (str "/products"))
+        (res/see-other (str "/products"))
         (ok [:h2  "product edit"]
             [:div "error"])))))
 
@@ -109,6 +110,10 @@
   (fn [{:keys [path-params]}]
     (let [{:keys [status]} (products/delete-product host (:product-id path-params))]
       (if (= 204 status)
-        (see-other (str "/products"))
+        (res/see-other (str "/products"))
         (ok [:h2 "product delete post"]
             [:div "error"])))))
+
+
+#_(defn get-new-products [host item-count]
+    (let [products (products/get-products host {:limit 5})]))
